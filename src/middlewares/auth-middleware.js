0581920ab1f3043276657fd213336e9c4ae0403c
José <eltitosetto@gmail.com>
2024-03-25 16:32:48 +0100

    auth-middleware formated

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -4,49 +4,49 @@ import logger from "../utils/logger.js";
 import User from '../models/user.js';
 import config from "../config.js";
 
-export function checkToken(req, res, next){
+export function checkToken(req, res, next) {
   console.log('[checkToken] Token:', req.headers.authorization)
 
-  const {authorization} = req.headers;
+  const { authorization } = req.headers;
 
-  if(!authorization) throw HttpStatusError(401, 'No token provided');
+  if (!authorization) throw HttpStatusError(401, 'No token provided');
 
   const [_bearer, token] = authorization.split(' ');
 
-  try{
-      const objectToken =jwt.verify(token, config.app.secretKey);
-      req.user = objectToken;
-  }catch(err){
-      logger.error(err.message);
-      throw HttpStatusError(401, 'Invalid token');
+  try {
+    const objectToken = jwt.verify(token, config.app.secretKey);
+    req.user = objectToken;
+  } catch (err) {
+    logger.error(err.message);
+    throw HttpStatusError(401, 'Invalid token');
   }
 
   next();
 }
 
 export async function isAdmin(req, res, next) {
-try {
-  const { authorization } = req.headers;
-  console.log('[isAdmin] Token:', authorization);
+  try {
+    const { authorization } = req.headers;
+    console.log('[isAdmin] Token:', authorization);
 
-  if (!authorization) {
-    throw new HttpStatusError(401, 'No se proporcionó un token de autorización');
+    if (!authorization) {
+      throw new HttpStatusError(401, 'No se proporcionó un token de autorización');
+    }
+
+    const token = authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, config.app.secretKey);
+
+    const userId = decodedToken.id;
+    const user = await User.findById(userId);
+    if (user.role === 'admin') {
+      req.user = user;
+      return next();
+    }
+
+    throw new HttpStatusError(403, 'Acceso no autorizado');
+  } catch (error) {
+    logger.error(error.message);
+    next(error);
   }
-
-  const token = authorization.split(' ')[1];
-  const decodedToken = jwt.verify(token, config.app.secretKey);
-
-  const userId = decodedToken.id;
-  const user = await User.findById(userId);
-  if (user.role === 'admin') {
-    req.user = user;
-    return next();
-  }
-
-  throw new HttpStatusError(403, 'Acceso no autorizado');
-} catch (error) {
-  logger.error(error.message);
-  next(error);
-}
 }
 
