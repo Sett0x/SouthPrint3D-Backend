2c3c4c1561ea9c48a5cfceee0b4427049a485d75
José <eltitosetto@gmail.com>
2024-03-29 14:47:00 +0100

    user-functions
    - added logout function
    - modfied login function
    - can login with user or email
    - changed error response
    - virification of already loged in
    - changed expiration time of token

--- src/controllers/login-controller.js
+++ src/controllers/login-controller.js
@@ -5,37 +5,40 @@ import jwt from 'jsonwebtoken';
 import config from '../config.js';
 import User from '../models/user.js'; // Importa el modelo de usuario
 
-/*
-export function login(req, res, next){
-       const { username, password } = req.body;
-
-    const user = findUser(username);
-
-    if(user){
-        console.log(password, user.password);
-        if(bcrypt.compareSync(password, user.password)){
-            const userInfo = { id: user.id, name: user.name };
-            const jwtConfig = { expiresIn: 10 };
-            const token = jwt.sign(userInfo, config.app.secretKey, jwtConfig);
-            return res.send({token});
-        }
-    }
-
-    throw new HttpStatusError(401, 'Invalid credentials');
-}
-
-*/
-
 export async function login(req, res, next) {
-  const { username, password } = req.body;
+  const { usernameOrEmail, password } = req.body;
 
   try {
-      // Busca al usuario por su nombre de usuario en la base de datos
-      const user = await User.findOne({ username });
+      // Verificar si ya hay un token de usuario en la solicitud
+      const authorizationHeader = req.headers.authorization;
+      if (authorizationHeader) {
+          const token = authorizationHeader.split(' ')[1];
+          jwt.verify(token, config.app.secretKey, (err, decoded) => {
+              if (err) {
+                  // Si el token no es válido, continuamos con el inicio de sesión normalmente
+              } else {
+                  // Si el token es válido, significa que el usuario ya está autenticado
+                  throw new HttpStatusError(400, 'Ya has iniciado sesión');
+              }
+          });
+      }
+
+      // Validar la entrada
+      if (!usernameOrEmail || !password) {
+          throw new HttpStatusError(400, 'El nombre de usuario/correo electrónico y la contraseña son obligatorios');
+      }
+
+      // Buscar al usuario por su nombre de usuario o correo electrónico en la base de datos
+      const user = await User.findOne({
+          $or: [
+              { username: usernameOrEmail },
+              { email: usernameOrEmail }
+          ]
+      });
 
       // Si no se encuentra el usuario, devuelve un error de credenciales inválidas
       if (!user) {
-          throw new HttpStatusError(401, 'Invalid credentials');
+          throw new HttpStatusError(401, 'Credenciales inválidas');
       }
 
       // Compara la contraseña proporcionada con la contraseña almacenada
@@ -43,12 +46,12 @@ export async function login(req, res, next) {
 
       // Si las contraseñas no coinciden, devuelve un error de credenciales inválidas
       if (!isPasswordValid) {
-          throw new HttpStatusError(401, 'Invalid credentials');
+          throw new HttpStatusError(401, 'Credenciales inválidas');
       }
 
       // Si las credenciales son válidas, genera un token JWT
       const userInfo = { id: user._id, username: user.username }; // Ajusta la información del usuario según tus necesidades
-      const token = jwt.sign(userInfo, config.app.secretKey, { expiresIn: '10h' }); // Ajusta el tiempo de expiración según tus necesidades
+      const token = jwt.sign(userInfo, config.app.secretKey, { expiresIn: '1h' }); // Ajusta el tiempo de expiración según tus necesidades
 
       // Envía el token JWT en la respuesta al cliente
       res.json({ token });
--- src/controllers/logout-controller.js
+++ src/controllers/logout-controller.js
@@ -2,13 +2,6 @@ import jwt from 'jsonwebtoken';
 import config from '../config.js';
 import { HttpStatusError } from 'common-errors';
 
-/**
- * Controlador para manejar el logout de un usuario.
- * Este controlador se encarga de invalidar el token de autenticación del usuario.
- * @param {import('express').Request} req - Objeto de solicitud de Express.
- * @param {import('express').Response} res - Objeto de respuesta de Express.
- * @param {import('express').NextFunction} next - Función de siguiente middleware.
- */
 export async function logout(req, res, next) {
   const token = req.headers.authorization?.split(' ')[1];
 
@@ -17,18 +10,13 @@ export async function logout(req, res, next) {
   }
 
   try {
-    // Decodificar el token para obtener la información del usuario
-    const decodedToken = jwt.verify(token, config.app.secretKey);
+    // Verificar si el token proporcionado es válido
+    jwt.verify(token, config.app.secretKey);
 
-    // Aquí podrías realizar cualquier acción adicional necesaria antes de invalidar el token,
-    // como registrar el evento de logout en una base de datos de auditoría, por ejemplo.
-
-    // No es necesario invalidar realmente el token en el servidor,
-    // ya que la autenticación basada en token es stateless.
-    // Simplemente respondemos con un mensaje de éxito y un token vacío.
-    res.status(200).json({ message: 'Logout successful', token: null });
+    // Si el token es válido, establecer el token de autorización como vacío para invalidar el token anterior y "cerrar sesión"
+    res.setHeader('Authorization', '');
+    res.status(200).json({ message: 'Logged out successfully' });
   } catch (error) {
-    // Si hay un error al verificar el token, pasa al siguiente middleware para su manejo
     next(new HttpStatusError(401, 'Token inválido'));
   }
 }
